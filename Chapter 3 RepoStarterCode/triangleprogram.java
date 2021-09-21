be2c0fd3f134e470b557233972268f93cc3c29b9
Sarayu <89226972+ssarayu25@users.noreply.github.com>
2021-09-21 17:49:38 -0500

    finished up triangle program

--- Chapter 3 RepoStarterCode/triangleprogram.java
+++ Chapter 3 RepoStarterCode/triangleprogram.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
  * Sarayu Suresh
  * 9/21/21
  * triangleprogram
- * Given the lengths of three sides of a triangle, print whether the triangle is scalene, isosceles, or equilateral.
+ * 
+ * Give the user the option to enter sides or angles of the triangle.The lengths will test whether 
+ * the triangle is scalene, isosceles, or equilateral.The angles will test to see if it is a right, 
+ * isosceles or equilateral.
  */
 public class triangleprogram
 {
@@ -24,17 +27,20 @@ public class triangleprogram
       int angle2 = in.nextInt();
       int angle3 = in.nextInt();
     
-      if (((angle1 + angle2 + angle3) == 180) && (angle1 != 0) && (angle2 != 0) && (angle3 != 0))
+      if (((angle1 + angle2 + angle3) == 180) && (angle1 > 0) && (angle2 > 0) && (angle3 > 0))
       {
+          
           if (angle1 == angle2 && angle2 == angle3)
             triangle = "equilateral";
           else if ((angle1 == angle2) || (angle2 == angle3) || (angle3 == angle1))
             triangle = "isosceles";
-          else 
-            triangle = "scalene";
+          if (angle1 == 90 || angle2 == 90 || angle3 == 90)
+            triangle += " right";
       }
       else
-        triangle = "invalid";
+        {triangle = "invalid";}
+      System.out.println("this is a " + triangle + " triangle");
+
     }
   
     else if (option == 2)
@@ -44,7 +50,7 @@ public class triangleprogram
       int side2 = in.nextInt();
       int side3 = in.nextInt();
     
-      if ((side1 + side2) > side3 && (side2 + side3) > side1 && (side3 + side1) > side2)
+      if ((side1 + side2) > side3 && (side2 + side3) > side1 && (side3 + side1) > side2 && (side1 > 0) && (side2 > 0) &&(side3 > 0))
       {
           if (side1 == side2 && side2 == side3)
             triangle = "equilateral";
@@ -55,10 +61,11 @@ public class triangleprogram
       }
       else
         triangle = "invalid";
+    System.out.println("this is a " + triangle + " triangle");
+
     }
     else
         System.out.print("invalid input");
-    System.out.println("this is a " + triangle + " triangle");
     }
     
 }
