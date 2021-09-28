48030cd1a48a5d917201c28469b89a5b31e17446
Sarayu <89226972+ssarayu25@users.noreply.github.com>
2021-09-28 15:10:42 -0500

    finished taxes, started minivan

---
+++ Chapter 3 RepoStarterCode/minivan.java
@@ -0,0 +1,15 @@
+//Sarayu Suresh
+//minivan
+//ckecks to see if a minivan's door is open
+//9/28/21
+
+import java.util.Scanner;
+public class minivan
+{
+        public static void main (String[] args)
+   {
+            Scanner Keyboard = new Scanner(System.in);
+            
+    }
+
+}
--- Chapter 3 RepoStarterCode/taxes.java
+++ Chapter 3 RepoStarterCode/taxes.java
@@ -14,19 +14,19 @@ public class taxes
       System.out.println("Enter yearly income: ");
       double income = in.nextDouble();
       double tax;
-     
+      // figuring out tax, dependent on user input
       if (income <= 50000)
         tax = income * 0.01;
       else if (income <= 75000)
         tax = 50000 * 0.01 + (income - 50000) * 0.02;
       else if (income <= 100000)
-        tax = 50000 * 0.01 + 25000 * 0.02 + (income - 75000) * 0.03;
+        tax = 50000 * 0.01 + (75000 - 50000) * 0.02 + (income - 75000) * 0.03;
       else if (income <= 250000)
-        tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + (income - 250000) * 0.04;
+        tax = 50000 * 0.01 + (75000 - 50000) * 0.02 + (100000 - 75000) * 0.03 + (income - 100000) * 0.04;
       else if (income <= 500000)
-        tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + 25000 * 0.04 + (income - 500000)* 0.05;
+        tax = 50000 * 0.01 + (75000 - 50000) * 0.02 + (100000 - 75000) * 0.03 + (250000 - 100000) * 0.04 + (income - 250000)* 0.05;
       else 
-        tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + 25000 * 0.04 + (income-600000) * 0.06;
+        tax = 50000 * 0.01 + (75000 - 50000) * 0.02 + (100000 - 75000) * 0.03 + (250000 - 100000) * 0.04 + (500000 - 250000) * 0.05 +(income-500000) * 0.06;
         
       
       System.out.println("Your tax total is " + tax);
