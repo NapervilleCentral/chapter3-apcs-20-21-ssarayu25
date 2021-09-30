5480504682919ddb47c5b00bdb045f8dd9bdce2c
Sarayu <89226972+ssarayu25@users.noreply.github.com>
2021-09-29 20:44:08 -0500

    debugged minivan program

--- Chapter 3 RepoStarterCode/minivan.java
+++ Chapter 3 RepoStarterCode/minivan.java
@@ -17,10 +17,10 @@ public class minivan
             char dashright = input.charAt(1);
             char childlock = input.charAt(2);
             char masterunlock = input.charAt(3);
-            char inright = input.charAt(4);
-            char inleft = input.charAt(5);
-            char outright = input.charAt(6);
-            char outleft = input.charAt(7);
+            char inleft = input.charAt(4);
+            char inright = input.charAt(5);
+            char outleft = input.charAt(6);
+            char outright = input.charAt(7);
             char gear = input.charAt(8);
             
             //both doors start off closed
@@ -28,29 +28,29 @@ public class minivan
             int right = 0; // 1 means open, while 0 means closed
             
             //algorithim to check which doors open
-            if ((masterunlock == 1) && ((gear == 'p')||(gear == 'P')))
-                {if (childlock == 0)
-                    {if (dashleft == 1 || inleft == 1 || outleft == 1)
+            if ((masterunlock == '1') && (gear == 'P'))
+                {if (childlock == '0')
+                    {if (dashleft == '1' || inleft == '1' || outleft == '1')
                         left = 1; 
-                     if (dashright == 1 || inright == 1 || outright == 1)
+                     if (dashright == '1' || inright == '1' || outright == '1')
                         right = 1;}
-                else if (childlock == 1)
-                    {if (dashleft == 1 || outleft == 1) 
+                else if (childlock == '1')
+                    {if (dashleft == '1' || outleft == '1') 
                         left = 1;
-                    if (dashright == 1 || outright == 1)
+                    if (dashright == '1' || outright == '1')
                         right = 1;}
         
             }
             
             
-            if (left == 1 && right == 1)
-                System.out.println("both doors open");
-            else if (left == 1 && right == 0)
-                System.out.println("left door opens");
-            else if (left == 0 && right == 1)
-                System.out.println("right door opens");
-            else if (left == 0 && right == 0)
-                System.out.println("both doors stay closed");
+            if (left == '1' && right == '1')
+                System.out.println("The right door is open and the left door is open.");
+            else if (left == '1' && right == '0')
+                System.out.println("The right door is closed and the left door is open.");
+            else if (left == '0' && right == '1')
+                System.out.println("The right door is open and the left door is closed.");
+            else if (left == '0' && right == '0')
+                System.out.println("The right door is closed and the left door is closed.");
     }
 
 }
