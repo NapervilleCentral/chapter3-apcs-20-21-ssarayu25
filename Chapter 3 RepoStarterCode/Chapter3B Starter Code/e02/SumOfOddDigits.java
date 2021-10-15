32851aff2b3e26320fa46b071210a7a9a4cefdd4
Sarayu <89226972+ssarayu25@users.noreply.github.com>
2021-10-15 16:20:47 -0500

    sum of odd digits

--- Chapter 3 RepoStarterCode/Chapter3B Starter Code/e02/SumOfOddDigits.java
+++ Chapter 3 RepoStarterCode/Chapter3B Starter Code/e02/SumOfOddDigits.java
@@ -9,13 +9,23 @@ public class SumOfOddDigits
       Scanner in = new Scanner(System.in);
 
       // Get the input
-
+      System.out.println("Enter a number: ");
+     
+      String input = in.next();
+      int total = 0;
+     
       // The sum of odd digits of the input
-
-         // Only add digit if it is odd
-
-
+      for (int i = 0; i < input.length(); i++)
+       {
+          String digit = input.substring(i, i+1);
+          int num = Integer.parseInt(digit);  
+          // Only add digit if it is odd
+          if (num % 2 != 0)
+            total += num;
+       }
+       
       // Output the result
+      System.out.print("Sum of odd digits: " + total);
 
    }
 }
