a7d1b455a534bc70702709a9f426e5c2b6ee295e
Sarayu <89226972+ssarayu25@users.noreply.github.com>
2021-10-19 15:06:16 -0500

    started program 3_12

---
+++ Chapter 3 RepoStarterCode/Chapter3B Starter Code/program3_12.java
@@ -0,0 +1,31 @@
+
+/**
+ * 3_12
+ * create triangle with *
+ *
+ * sarayu suresh
+ * 10/19/21
+ */
+
+
+public class program3_12
+{
+    public static void main (String[] args)
+    {
+        for (int i = 1; i < 11; i++)
+            {
+            for (int j = 0; j < (11-i); j ++)
+                System.out.print(" ");
+            for (int j = 0; j < i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+        
+        
+        
+        for (int i = 1; i < 11; i++)
+        {
+            for (int j = 0; j <
+        }
+        }
+}
