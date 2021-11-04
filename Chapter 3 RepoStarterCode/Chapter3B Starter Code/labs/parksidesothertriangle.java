3d8126beb1b29df6f648f33a5c499c6be31f5bfd
Sarayu <89226972+ssarayu25@users.noreply.github.com>
2021-11-03 21:35:45 -0500

    finally finished parksides other triangle
    YAYYYYYY!!!

--- Chapter 3 RepoStarterCode/Chapter3B Starter Code/labs/parksidesothertriangle.java
+++ Chapter 3 RepoStarterCode/Chapter3B Starter Code/labs/parksidesothertriangle.java
@@ -1,10 +1,7 @@
 package labs;
 import java.util.Scanner;
-
-
 /**
- * input size and seed to get last number in pattern
- *
+ * input size and seed to get parksides other triangle
  * sarayu suresh
  * 10/30/21
  */
@@ -16,18 +13,23 @@ public class parksidesothertriangle
         System.out.println("enter size and seed (size first)");
         int size = gen.nextInt();
         int seed = gen.nextInt();
-          
+        int sumy = 0;
         
-        for (int y = 0; y < size; y++)
+        for (int y = 0; y < size; y++) //y is the row number 
         {
-            for (int i = 0; i < y; i++)
+            for (int i = 0; i < y; i++) //prints out spaces (starts with zero and goes up)
                 System.out.print(" ");
-            int print = seed + y;
-            if (print > 9)
-                 print = print % 9;
-            for (int x = y; x < size; x++)
+                
+            sumy = sumy + y; //rolling sum of the y values
+            // in each line the starting value is the seed plus all the row numbers before it
+            int print = seed + sumy;  
+            
+            if (print > 9) //since it goes from 9 to 1
+                 print = print % 9;   
+                 
+            for (int x = y; x < size; x++) //x is the column number
             {
-                print = print + x;
+                print = print + x; //goes up by number that increases by one 
                 if (print > 9)
                  print = print % 9;
                 System.out.print(print);    
